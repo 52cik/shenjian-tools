27b6ebb98b0eb00fe27be79bae42f330b0b61932
楼教主 <cik520@qq.com>
2018-03-22 23:21:08 +0800

    添加工具方法

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,6 @@
-var tools=function(t){"use strict";var n=1;return t.test=function(){return"my test"},t.add=function(){return n},t}({});
+/*!
+ * shenjian tools v1.0.0
+ * (c) 2018 楼教主 <fe.52cik@gmail.com> (http://www.52cik.com/)
+ * Released under the MIT License.
+ */
+var tools=function(t){"use strict";return t.getTime=function(t){var e="";if(t.replace(/(20\d+)\D(\d+)\D(\d+)(?:\D+(\d+):(\d+)(?::(\d+))?)?/,function(t,n,r,d,u,c,i){e=n+"-"+r+"-"+d,u&&(e+=" "+u),c&&(e+=":"+c),i&&(e+=":"+i)}),!/^20\d+-\d+-\d+/.test(e))return null;e=e.replace(/(?=\b\d\b)/g,"0");var n="0000-00-00 00:00:00".slice(e.length);return e+=n,new Date(e)},t.test=function(){},t}({});
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-export function test() {
-  return 'my test';
+/**
+ * 获取字符串中的日期
+ *
+ * @export
+ * @param {string} str 含有日期的字符串
+ * @return {Date}
+ */
+export function getTime(str) {
+  const re = /(20\d+)\D(\d+)\D(\d+)(?:\D+(\d+):(\d+)(?::(\d+))?)?/;
+  let date = '';
+
+  str.replace(re, (all, y, m, d, h, i, s) => {
+    date = `${y}-${m}-${d}`;
+    if (h) date += ` ${h}`;
+    if (i) date += `:${i}`;
+    if (s) date += `:${s}`;
+  });
+
+  if (!/^20\d+-\d+-\d+/.test(date)) {
+    return null;
+  }
+
+  date = date.replace(/(?=\b\d\b)/g, '0');
+
+  const pad = '0000-00-00 00:00:00'.slice(date.length);
+
+  date += pad;
+  return new Date(date);
 }
 
-const a = 1;
-export function add() {
-  return a;
-}
+export function test() {}
