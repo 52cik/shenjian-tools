a4b35353d81ddddd590b56b01403527214ee0ef4
楼教主 <cik520@qq.com>
2018-03-25 19:27:12 +0800

    优化代码

--- src/string.js
+++ src/string.js
@@ -1,10 +1,17 @@
-// 解析 json jsonp 数据
-export function getJSON(str) {
+/**
+ * 解析 json jsonp 数据
+ *
+ * @export
+ * @param {string} str 需要解析的字符串
+ * @param {object|array} def 默认返回
+ * @returns
+ */
+export function getJSON(str, def) {
   str = str.replace(/^[^{[]+/, '').replace(/[^}\]]+$/, '');
   try {
     return JSON.parse(str);
   } catch (error) {
-    return {};
+    return def || {};
   }
 }
 
