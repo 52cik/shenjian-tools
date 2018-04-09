9d958b2c9d4eb7e1078b3b7b2896e2abb660561b
楼教主 <cik520@qq.com>
2018-04-09 20:34:55 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,9 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     'no-param-reassign': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-template': 'off',
+    'func-names': 'off',
+    'no-use-before-define': 'off',
   },
 };
