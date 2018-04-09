056965c41e84d5c4c7c050bb050e753a7eb5ad0f
楼教主 <cik520@qq.com>
2018-04-09 20:35:06 +0800

    添加工具方法

--- src/string.js
+++ src/string.js
@@ -23,7 +23,33 @@ export function getJSON(str, def) {
  */
 export function clearImg(str) {
   return str
-    .replace(/<img[^>]+?src="([^"]+)"[^>]*>(?:<\/img>)?/gi, '<img src="$1">');
+    .replace(/<img[^>]+?src="([^"]+)"[^>]*>(?:<\/img>)?/gi, function (m, src) {
+      return '<img src="' + fixImg(src) + '">';
+    });
+}
+
+/**
+ * 修复图片后缀
+ *
+ * @export
+ * @param {string} url url地址
+ */
+export function fixImgExt(url) {
+  // 第一财经类型带@参数图片
+  // http://imgcdn.yicai.com/uppics/slides/2018/04/636588982981500913.png@250w_1e_1c
+  if (/\.(jpe?g|gif|a?png|bmp|webp)@/.test(url)) {
+    // 636588982981500913.png@250w_1e_1c
+    // 636588982981500913.png@250w_1e_1c.png
+    return url.replace(/(\.\w+)@.+/, '$&$1');
+  }
+
+  // 有后缀就放行 （未知情况在说）
+  if (/\.(jpe?g|gif|a?png|bmp|webp)/.test(url)) {
+    return url;
+  }
+
+  // 腾讯娱乐类型无后缀图片
+  return url.replace(/(\?.+)?$/, '.jpg$&');
 }
 
 /**
@@ -35,4 +61,15 @@ export function clearImg(str) {
  */
 export function fixHttp(url) {
   return url.replace(/(?=^\/\/)/, 'http:');
+}
+
+/**
+ * 修复 相对协议地址 + 修复图片后缀
+ *
+ * @export
+ * @param {string} url url地址
+ * @returns
+ */
+export function fixImg(url) {
+  return fixImgExt(fixHttp(url));
 }
