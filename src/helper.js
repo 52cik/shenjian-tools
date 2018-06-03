858992c61bf59b93e22c3501408b90ab91b4ed72
楼教主 <cik520@qq.com>
2018-06-03 16:57:08 +0800

    添加业务工具

---
+++ src/helper.js
@@ -0,0 +1,16 @@
+/**
+ * 业务工具集合
+ */
+
+/**
+ * 是否为深度文章
+ * @param {number} length 文字字数
+ * @return {boolean}
+ */
+export function isDeep(length) {
+  return length > 1700;
+}
+
+export function test() {
+  // noop
+}
