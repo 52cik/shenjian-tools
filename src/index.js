1fcab5d97c62bdc031150bbc87fb884e324cd2eb
楼教主 <cik520@qq.com>
2018-06-03 16:57:29 +0800

    整理导出入口

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import store from './store'; // 缓存处理
+import * as helper from './helper'; // 业务工具
+
 export * from './date'; // 日期处理
 export * from './string'; // 字符串处理
-export { default as store } from './store'; // 缓存处理
+
+export { store, helper };
