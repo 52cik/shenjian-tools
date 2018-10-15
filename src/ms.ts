619c30de5338e515d95a166f20a3bcb0b3faf78c
楼教主 <cik520@qq.com>
2018-10-15 21:27:20 +0800

    optimization code

--- src/ms.ts
+++ src/ms.ts
@@ -5,7 +5,7 @@ const d = h * 24;
 const w = d * 7;
 const y = d * 365.25;
 
-const re = /^((?:\d+)?-?\d?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i;
+const re = /^((?:\d+)?-?\d?\.?\d+) *(ms|s|m|h|d|w|y)?$/i;
 
 /**
  * Parse the given `str` and return milliseconds.
@@ -25,45 +25,20 @@ export default function parse(str: string): number {
   const n = parseFloat(match[1]);
   const type = (match[2] || 'ms').toLowerCase();
   switch (type) {
-    case 'years':
-    case 'year':
-    case 'yrs':
-    case 'yr':
     case 'y':
       return n * y;
-    case 'weeks':
-    case 'week':
     case 'w':
       return n * w;
-    case 'days':
-    case 'day':
     case 'd':
       return n * d;
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
     case 'h':
       return n * h;
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
     case 'm':
       return n * m;
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
     case 's':
       return n * s;
-    case 'milliseconds':
-    case 'millisecond':
-    case 'msecs':
-    case 'msec':
     case 'ms':
       return n;
-    /* istanbul ignore next */
     default:
       return 0;
   }
