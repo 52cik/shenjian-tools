65fbdc47d8ec4d2ffb2fe34924ac3aeba3238089
楼教主 <cik520@qq.com>
2019-01-06 23:18:50 +0800

    2.1.0

--- src/helper.ts
+++ src/helper.ts
@@ -1,11 +1,21 @@
 /**
  * 业务工具集合
  */
+import { clearImg } from './string';
+import { entities } from './entities';
 
 /**
  * 是否为深度文章
  * @param length 文字字数
  */
-export function isDeep(length: number): boolean {
+export function isDeep(length: number) {
   return length > 1700;
+}
+
+/**
+ * 内容通用过滤
+ * @param str 字符串
+ */
+export function content(str: string) {
+  return clearImg(entities(str));
 }
